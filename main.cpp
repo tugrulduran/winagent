ac62593e7e29d65a6af87d1c73335038a915acbd
Tugrul Duran <duran.tugrul@gmail.com>
2026-02-24 21:27:19 +0300

    Bump application version to 0.3.0

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     app.setApplicationName("WinAgent Dashboard");
-    app.setApplicationVersion("0.2.0");
+    app.setApplicationVersion("0.3.0");
     app.setWindowIcon(QIcon("app_icon.ico"));
     app.setQuitOnLastWindowClosed(false);
 
